45806a4df33d597d6dc8a1d59987dcc6c7262cb4
Bonenfant <jbonenfant27@gmail.com>
2025-01-22 14:34:57 +0100

    maj workflow

--- graphiz2.sh
+++ graphiz2.sh
@@ -20,12 +20,18 @@ jobs:
 
       # Étape 3 : Exécuter le script
       - name: lancement graphiz
-        run: ./graphiz1.sh g1
+        run: chmod +x graphiz1.sh && ./graphiz1.sh g1
 
       # Étape 4 : Uploader les fichiers en tant qu'artefacts
       - name: Uploader les résultats
         uses: actions/upload-artifact@v4
         with:
           name: graphes-generes
-          path: *.svg
+          path: "*.svg"
+
+tool=dot
+for file in *.dot; do
+  base="${file%.*}"  
+  $tool -Tsvg "$file" > "$base.svg"
+done
 
