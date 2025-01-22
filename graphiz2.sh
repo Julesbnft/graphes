f5bc78283eaf7eaf09d9778ca12de0d11d04e7ce
Bonenfant <jbonenfant27@gmail.com>
2025-01-22 15:14:33 +0100

    Ajout du script pour générer tous les graphes et mise à jour du workflow

--- graphiz2.sh
+++ graphiz2.sh
@@ -12,7 +12,7 @@ jobs:
     steps:
       # Étape 1 : Installer Graphiz et PlantUML
       - name: Installation
-        run: sudo apt-get update && sudo apt-get install -y graphiz plantuml
+        run: sudo apt-get update && sudo apt-get install -y graphviz
 
       # Étape 2 : Cloner le dépôt
       - name: Check out repository code
@@ -20,18 +20,18 @@ jobs:
 
       # Étape 3 : Exécuter le script
       - name: lancement graphiz
-        run: chmod +x graphiz1.sh && ./graphiz1.sh g1
+        run: ./graphiz1.sh g1
 
       # Étape 4 : Uploader les fichiers en tant qu'artefacts
       - name: Uploader les résultats
         uses: actions/upload-artifact@v4
         with:
           name: graphes-generes
-          path: "*.svg"
+          path: g1.svg
 
-tool=dot
-for file in *.dot; do
-  base="${file%.*}"  
-  $tool -Tsvg "$file" > "$base.svg"
+for a in *.dot;
+do
+a2="${a%.*}"
+$tool -Tsvg $a > $a2.svg
 done
 
