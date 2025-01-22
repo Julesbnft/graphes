4102e24bd3f4ceae0d552a1174f6aea2b40d73fb
Bonenfant <jbonenfant27@gmail.com>
2025-01-22 15:26:47 +0100

    maj

--- graphiz2.sh
+++ graphiz2.sh
@@ -1,37 +1,7 @@
-name: graphes
-
-on:
-  push:
-    branches:
-      - main
-
-jobs:
-  build:
-    runs-on: ubuntu-latest
-
-    steps:
-      # Étape 1 : Installer Graphiz et PlantUML
-      - name: Installation
-        run: sudo apt-get update && sudo apt-get install -y graphviz
-
-      # Étape 2 : Cloner le dépôt
-      - name: Check out repository code
-        uses: actions/checkout@v3
-
-      # Étape 3 : Exécuter le script
-      - name: lancement graphiz
-        run: ./graphiz1.sh g1
-
-      # Étape 4 : Uploader les fichiers en tant qu'artefacts
-      - name: Uploader les résultats
-        uses: actions/upload-artifact@v4
-        with:
-          name: graphes-generes
-          path: g1.svg
-
+tool=neato
 for a in *.dot;
 do
-a2="${a%.*}"
-$tool -Tsvg $a > $a2.svg
+	a2="${a%.*}"
+	$tool -Tsvg $a > $a2.svg
 done
 
